a2295cd20a15a68d58db785e09adb0ee8eee219c
Aishia <51677946+Aisha-22@users.noreply.github.com>
2021-08-24 22:16:49 +0000

    Exercise Updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Printing to the Console
 
+https://pythontutor.com/
+
 # Instructions
 
 Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function. 
--- main.py
+++ main.py
@@ -12,6 +12,23 @@ print("Hello world!\nHello Aisha\nHello Princess")
 # String concatenation
 print("Hello" + " " + "Aisha")
 
+#Fix the code below 👇
+# Debugging
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# The Python Input function 
+print("Hello " + input("What is your name?"))
+
+# Code Exercise - Input Function https://pythontutor.com/
+print(len(input("What is your name? ")))
+
+# Python Variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
 
 
 

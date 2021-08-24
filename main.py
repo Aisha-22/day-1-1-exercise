47822769b347b62ec662ed257d0a6d0bcb47f542
Aishia <51677946+Aisha-22@users.noreply.github.com>
2021-08-24 20:20:38 +0000

    Second Exercise

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #Write your code below this line 👇
-
+# [Interactive Coding Exercise] Printing
 print("Day 1 - Python Print Function")
 
 print("The function is declared like this:")
 
 print("print('what to print')")
+
+# String Manipulation and Code Intelligence
+print("Hello world!\nHello Aisha\nHello Princess")
+
+
 
 
 
